5b89c19c0a729d8a65bcd7d985012a624debf70c
jefry22 <jeffersonpierov@gmail.com>
2023-09-10 20:41:40 -0500

    practica

---
+++ js/index.js
@@ -0,0 +1,76 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+    el: '.swiper-pagination',
+  },
+    
+
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".swiper-tienda",{
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+    el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  })
+
+
+
+  var swiper = new Swiper(".slide-container-especial", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+    el: '.swiper-pagination',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+    },
+  });
